37c3584791261eb5cc164361ce5bd7766d09cfce
William Carlström <vbw123@live.se>
2023-05-11 21:12:44 +0200

    fixed eslint errors and changed size of input

--- front/src/components/Input.tsx
+++ front/src/components/Input.tsx
@@ -2,28 +2,25 @@ interface Props {
 	className: string;
 	placeHolder: string;
 	inputID: string;
-	
 
 	opacity?: number;
 	labelText?: string;
-	
+
 }
 
-const def = "box-border h-10 w-80 rounded border-solid border-gray-300 border";
+const def = "box-border h-11 w-72 rounded border-solid border-gray-300 border";
 
 export default function Input(props:Props)
 {
 
-	
 	return (
-		
 		<div className="flex flex-col">
-		{props.labelText ? 
-		<label htmlFor={props.inputID}>{props.labelText}</label>
-		:
-		null}
-		<input style={{opacity: props.opacity}} id={props.inputID} placeholder={props.placeHolder} className={def + props.className}></input>
+			{props.labelText ?
+				<label htmlFor={props.inputID}>{props.labelText}</label>
+				:
+				null}
+			<input style={{opacity: props.opacity}} id={props.inputID} placeholder={props.placeHolder} className={def + props.className}></input>
 		</div>
-		
-	)
+
+	);
 }
