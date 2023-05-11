5c0eb0909e90d59c80fd94e07822dee4b71d331b
Cattis Gustafsson <cattis@hejdev.se>
2023-05-11 11:09:37 +0200

    Table works

--- front/src/pages/Conversation.tsx
+++ front/src/pages/Conversation.tsx
@@ -1,9 +1,39 @@
+import SortTable from "../components/SortTable";
+import { DataItem } from "../components/Table";
+
 function Conversation () {
-	return (
-		<>
-			<p>Hej</p>
-		</>
+	// FAKE DATA - ta bort när man har data
+	const data: DataItem[] = [
+		{dishes: "Maträtt 1", adress: "Adress 1 med gata 1", time: Date.now()},
+		{dishes: "Maträtt 2", adress: "Adress 2 med gata 2", time: Date.now()},
+		{dishes: "Maträtt 3", adress: "Adress 3 med gata 3", time: Date.now()},
+	];
+
+	const config = [
+		{
+			label: "Maträtt",
+			render: (vegetable: DataItem) => <>{vegetable.dishes}</>,
+			sortValue: (vegetable: DataItem) => vegetable.dishes
+		},
+		{
+			label: "Adress",
+			render: (vegetable: DataItem) => <>{vegetable.adress}</>,
+			sortValue: (vegetable: DataItem) => vegetable.adress
+		},
+		{
+			label: "Tid",
+			render: (vegetable: DataItem) => <>{vegetable.time}</>,
+			sortValue: (vegetable: DataItem) => vegetable.time
+		},
+	];
+	const getKey = (item: DataItem) => item.dishes || "";
+
+	return(
+		<div>
+			<SortTable data={data} config={config} getKey={getKey}></SortTable>
+		</div>
 	);
 }
+
 
 export default Conversation;
