1bc5a9e9c4651152f01aaa73ba93882494d70a95
Cattis Gustafsson <cattis@hejdev.se>
2023-05-12 12:34:09 +0200

    Started the design of chat page and the logic

--- front/src/pages/Chat.tsx
+++ front/src/pages/Chat.tsx
@@ -1,7 +1,43 @@
+import { FaAngleLeft, FaExclamation } from "react-icons/fa";
+
 function Chat() {
+	const props = {
+		title: "Maträttens rubrik",
+		buyer: "Köparens namn",
+		seller: true,
+		chat: [
+			{
+				id: 1,
+				user: 111,
+				msg: "Hej jag vill köpa din vara"
+			},
+			{
+				id: 2,
+				user: 212,
+				msg: "Okej du kan hämta klockan 14"
+			},
+			{
+				id: 3,
+				user: 111,
+				msg: "Hej jag vill köpa din vara"
+			},
+			{
+				id: 4,
+				user: 111,
+				msg: "Hej jag vill köpa din vara"
+			},
+		]
+	};
 	return(
-		<div>
-			Chat
+		<div className="w-10/12 mx-auto max-w-5xl">
+			<section className="mt-5 flex justify-between">
+				<FaAngleLeft className="text-2xl"></FaAngleLeft>
+				<div className="flex flex-col text-center">
+					<h2 className="text-xl font-semibold">{props.title}</h2>
+					<h3 className="text-lg">{props.buyer}</h3>
+				</div>
+				<FaExclamation className="text-xl text-red-700"></FaExclamation>
+			</section>
 		</div>
 	);
 }
