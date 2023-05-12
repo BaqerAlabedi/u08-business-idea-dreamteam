dd1a10f53d106a1be6c84e0f909bc1b6b3610935
luk240 <113014694+luk240@users.noreply.github.com>
2023-05-12 14:46:59 +0200

    Button.tsx white prop

--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -2,14 +2,16 @@ interface Props {
 	children: React.ReactNode;
 	onClick?: () => void;
 	red?: boolean;
+	white?: boolean;
 	className?: string;
 }
 
-const def = "border-transparent border-2 font-semibold rounded shadow-md ";
-export default function Button({children, onClick, red, className}:Props) {
+const def = "font-semibold rounded-sm shadow-md ";
+export default function Button({children, onClick, red, white, className}:Props) {
 	let style = (red)
-		? def + "border-red-700 bg-white hover:bg-red-700 text-black hover:text-white "
-		: def + "bg-teal-700 text-white hover:bg-teal-800 ";
+		? def + "border-2 border-red-700 bg-white hover:bg-red-700 text-black hover:text-white "
+		: (white) ? def + "border-2 border-teal-700 hover:border-none bg-white hover:bg-teal-800 hover:text-white "
+			: def + "bg-teal-700 text-white hover:bg-teal-800 ";
 	style += (className) ? className : "w-72 h-11";
 	return(
 		<button onClick={onClick} className={style}>{children}</button>
