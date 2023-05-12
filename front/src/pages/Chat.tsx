5be0d8f950b678f87796fb03358f8c4ea2add890
Cattis Gustafsson <cattis@hejdev.se>
2023-05-12 12:46:54 +0200

    Design of chat page

--- front/src/pages/Chat.tsx
+++ front/src/pages/Chat.tsx
@@ -9,35 +9,57 @@ function Chat() {
 			{
 				id: 1,
 				user: 111,
-				msg: "Hej jag vill köpa din vara"
+				msg: "Hej jag vill köpa din vara",
+				time: Date.now()
 			},
 			{
 				id: 2,
 				user: 212,
-				msg: "Okej du kan hämta klockan 14"
+				msg: "Okej du kan hämta klockan 14",
+				time: Date.now()
 			},
 			{
 				id: 3,
 				user: 111,
-				msg: "Hej jag vill köpa din vara"
+				msg: "Okej kan vi säga klockan 13? Eller blir det för tidigt för dig?",
+				time: Date.now()
 			},
 			{
 				id: 4,
 				user: 111,
-				msg: "Hej jag vill köpa din vara"
+				msg: "Jag antar att det blir för tidigt. Vi säger klockan 14",
+				time: Date.now()
 			},
 		]
 	};
-	return(
+
+	return (
 		<div className="w-10/12 mx-auto max-w-5xl">
 			<section className="mt-5 flex justify-between">
-				<FaAngleLeft className="text-2xl"></FaAngleLeft>
+				<FaAngleLeft className="text-2xl" />
 				<div className="flex flex-col text-center">
 					<h2 className="text-xl font-semibold">{props.title}</h2>
 					<h3 className="text-lg">{props.buyer}</h3>
 				</div>
-				<FaExclamation className="text-xl text-red-700"></FaExclamation>
+				<FaExclamation className="text-xl text-red-700" />
 			</section>
+			<section className="mt-5">
+				{props.chat.map((message) => (
+					<div
+						key={message.id}
+						className={`flex justify-${message.user === 111 ? "start" : "end"} mb-4`}
+					>
+						<div
+							className={`${
+								message.user === 111 ? "bg-teal-700/50" : "bg-gray-200"
+							} rounded-lg p-3 max-w-xs`}
+						>
+							{message.msg}
+						</div>
+					</div>
+				))}
+			</section>
+			<hr />
 		</div>
 	);
 }
