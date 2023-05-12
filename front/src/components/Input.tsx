296ceb7af3895a02f67339272dff35cd865fe13f
William Carlström <vbw123@live.se>
2023-05-12 13:13:30 +0200

    removed className component

--- front/src/components/Input.tsx
+++ front/src/components/Input.tsx
@@ -1,5 +1,4 @@
 interface Props {
-	className: string;
 	placeHolder: string;
 	inputID: string;
 
@@ -8,7 +7,7 @@ interface Props {
 
 }
 
-const def = "box-border h-11 w-72 rounded border-solid border-gray-300 border";
+const styling = "box-border h-11 w-72 rounded border-solid border-gray-300 border";
 
 export default function Input(props:Props)
 {
@@ -19,7 +18,7 @@ export default function Input(props:Props)
 				<label htmlFor={props.inputID}>{props.labelText}</label>
 				:
 				null}
-			<input style={{opacity: props.opacity}} id={props.inputID} placeholder={props.placeHolder} className={def + props.className}></input>
+			<input style={{opacity: props.opacity}} id={props.inputID} placeholder={props.placeHolder} className={styling}></input>
 		</div>
 
 	);
