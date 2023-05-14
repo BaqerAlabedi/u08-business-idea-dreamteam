ede06e7e749f5188bebcdaa8a042b28123b1bcde
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-05-14 17:50:45 +0200

    style ready

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -6,9 +6,9 @@ function Login() {
 		<div className="relative h-screen bg-cover bg-center bg-login ">
 			<div className="absolute rounded-md sm:w-1/2 md:w-1/3 h-3/5 lg:bg-white opacity-80 lg:p-12 top-1/2 left-2/4 transform -translate-x-1/2 -translate-y-1/2">
 				<div className="ml-2 flex flex-col justify-center justify-items-center mb-12">
-					<div className="lg:ml-8 mb-8">
+					<div className="lg:ml-8 mb-4">
 						<h1 className="mb-0 font-bold">Logga in</h1><br></br>
-						<h2 className="w-48 mt-0 mb-4">till GrannskapsRätten</h2>
+						<h2 className="w-48 mt-0 mb-2">till GrannskapsRätten</h2>
 					</div>
 					<form className="lg:ml-12 ">
 						<Input inputID="email" labelText="Email" opacity={1} placeHolder=""></Input>
