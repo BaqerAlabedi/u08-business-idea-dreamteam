f01dafdfeb146de37e13afcef2b4710024801b93
luk240 <113014694+luk240@users.noreply.github.com>
2023-05-15 01:55:06 +0200

    Navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,11 @@
 import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 import ProductShow from "./components/ProductShow";
 
 function App() {
 	return (
 		<>
+			<Navbar/>
 			<h1 className="underline">Frontend</h1>
 			<Button onClick={() => console.log("hello")} className="w-32 h-8">Def+Class btn</Button>
 			<Button onClick={() => console.log("hello")} red>Red btn</Button>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "drop": {
+          "0%": {transform: "scaleY(0) translateY(-100px)", filter: "opacity(0) blur(10px)", border: "solid 20px black"},
+          "70%": {transform: "scaleY(1.1)"}
+        },
+        "poof": {"100%": {filter: "opacity(0) blur(50px);", transform: "scale(1.5)"}}
+      },
+      animation: {
+      "drop": "drop .5s ease-in-out forwards",
+      "poof": "poof .3s ease-in-out forwards"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
