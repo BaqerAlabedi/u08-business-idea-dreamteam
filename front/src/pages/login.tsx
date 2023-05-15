1294621b9a0f99611f4bac60b4e34882043e2b4d
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-05-15 14:51:56 +0200

    done

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -4,7 +4,7 @@ import Input from "../components/Input";
 function Login() {
 	return (
 		<div className="flex items-center justify-center h-screen bg-cover bg-center bg-login ">
-			<div className="rounded-md md:w-1/3 h-3/5 lg:bg-white/80 lg:p-12 ">
+			<div className="rounded-md md:w-1/3  lg:bg-white/80 lg:p-12 ">
 				<div className="flex flex-col justify-center mb-12">
 					<div className="mx-auto">
 						<h1 className=" text-lg w-72 font-bold">Logga in</h1>
