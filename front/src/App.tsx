c5e4e11fd36e2182d880239d7911e209446d1dab
salleeb <saliem.brhane@chasacademy.se>
2023-05-15 15:54:12 +0200

    styling

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import Button from "./components/Button";
 import ProductShow from "./components/ProductShow";
+import Search from "./components/Search";
 
 function App() {
 	return (
 		<>
 			<h1 className="underline">Frontend</h1>
 			<Button onClick={() => console.log("hello")} className="bg-red-700 text-white">Example btn</Button>
+			<Search name="Sök efter plats" filter/>
 			<ProductShow></ProductShow>
 		</>
 	);
