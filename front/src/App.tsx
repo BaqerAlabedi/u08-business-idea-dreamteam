365d3f114f01439f3bf702493f5fea72c8decab7
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-05-15 08:31:29 +0200

    auth frontend done

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,9 @@
-// import Button from "./components/Button";
-// import ProductShow from "./components/ProductShow";
 import Register from "./pages/Register";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 function App() {
 	return (
 		<>
-			{/* <h1 className="underline">Frontend</h1>
-			<Button onClick={() => console.log("hello")} className="bg-red-700 text-white">Example btn</Button> */}
 			<Login></Login>
 			<Register></Register>
 		</>
