edda29d8dcd33a9281e6d77fc0e830ed444f4de8
salleeb <saliem.brhane@chasacademy.se>
2023-05-16 13:42:54 +0200

    removed import

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,5 @@
 import Button from "./components/Button";
 import ProductShow from "./components/ProductShow";
-import { EditProfile } from "./pages/EditProfile";
 
 function App() {
 	return (
@@ -8,7 +7,6 @@ function App() {
 			<h1 className="underline">Frontend</h1>
 			<Button onClick={() => console.log("hello")} className="bg-red-700 text-white">Example btn</Button>
 			<ProductShow></ProductShow>
-			<EditProfile></EditProfile>
 		</>
 	);
 }
