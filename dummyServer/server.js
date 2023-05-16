a3753d2b4c74d92dee655f9e6fe2b67d4eb63d8d
Cattis Gustafsson <cattis@hejdev.se>
2023-05-16 12:26:55 +0200

    First dummy draft

--- dummyServer/server.js
+++ dummyServer/server.js
@@ -7,75 +7,337 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/books", async (req, res) => {
+app.get("/products/:userID", async (req, res) => {
     res.status(200).json({
         "message": "success",
-        "books": [
+        "user":
             {
                 "_id": "645b7cc3424d43eee1ea75ff",
-                "title": "Blue room",
-                "author": "Sebbe",
-                "publisher": "Lindcode Books",
-                "read": true,
-                "__v": 0
-            },
-            {
-                "_id": "645b7ce0424d43eee1ea7601",
-                "title": "Red room",
-                "author": "Mr Lindgren",
-                "publisher": "Lindcode Books",
-                "read": true,
-                "__v": 0
-            },
-            {
-                "_id": "645b7ce0424d43eee1ea7602",
-                "title": "Yellow room",
-                "author": "Mr Tomas",
-                "publisher": "Lindcode Books (tm)",
-                "read": false,
-                "__v": 0
+                "first_name": "Cattis",
+                "surname": "Gustafsson",
+                "email": "cattis@hejdev.se",
+				"address": "Arenavägen 63",
+				"img": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+				"foods": [{
+					"_id": "AAAA",
+					"title": "Food title",
+					"desc": "Lorem ipsum",
+					"location": [45.123, 47.232],
+					"free": false,
+					"price": 20,
+					"img": "food.jpg",
+					"expire": ["Tillagningsdatum", Date.now()],
+					"tags": ["vegan", "middag", "hemmagjord", "soppa"],
+					"sold_to": "222222222222222222222222",
+					"conversation": [
+						{
+							"conversation_id": 12345,
+							"time": Date.now(),
+							"buyer": "222222222222222222222222",
+							"messages": [
+								{
+									"sender": "user1",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								}
+							]
+						}
+					]
             }
         ]
-    })
+    }})
 })
 
-app.get("/books/645b7cc3424d43eee1ea75ff", async (req, res) => {
+
+
+app.get("/users", async (req, res) => {
     res.status(200).json({
-        "message": "success",
-        "book": {
-            "_id": "645b7cc3424d43eee1ea75ff",
-            "title": "Blue room",
-            "author": "Sebbe",
-            "publisher": "Lindcode Books",
-            "read": true,
-            "__v": 0
-        }
+        "message": "success login",
     });
 })
 
-app.post("/books", async (req, res) => {
+app.post("/users", async (req, res) => {
     res.status(201).json({
-        "message": "New book record created"
+        "message": "Registration successfull"
     })
-})
+}) 
 
-app.put("/books/645b7cc3424d43eee1ea75ff", async (req, res) => {
+app.get("/products/:productID", async (req, res) => {
     res.status(200).json({
         "message": "success",
-        "book": {
-            "_id": "645b7cc3424d43eee1ea75ff",
-            "title": "Yellow room",
-            "author": "Sebbe",
-            "publisher": "Lindcode Books",
-            "read": false,
-            "__v": 0
-        }
+        "foods": {
+			"_id": "AAAA",
+			"title": "Food title",
+			"desc": "Lorem ipsum",
+			"location": [45.123, 47.232],
+			"free": false,
+			"price": 20,
+			"img": "food.jpg",
+			"expire": ["Tillagningsdatum", Date.now()],
+			"tags": ["vegan", "middag", "hemmagjord", "soppa"],
+			"sold_to": "222222222222222222222222",
+			"conversation": [
+				{
+					"conversation_id": 12345,
+					"time": Date.now(),
+					"buyer": "222222222222222222222222",
+					"messages": [
+						{
+							"sender": "user1",
+							"message": "Hello World",
+							"timestamp": Date.now()
+						},
+						{
+							"sender": "user1",
+							"message": "Hello",
+							"timestamp": Date.now()
+						},
+						{
+							"sender": "user2",
+							"message": "Hello World",
+							"timestamp": Date.now()
+						},
+						{
+							"sender": "user1",
+							"message": "Hello",
+							"timestamp": Date.now()
+						}
+					]
+				}
+			]
+	}
     })
 })
 
-app.delete("/books/645b7cc3424d43eee1ea75ff", async (req, res) => {
+// Chatten
+
+app.get("/conversations/:productID/:userID", async (req, res) => {
     res.status(200).json({
-        "message": "Deleted Successfully"
+        "message": "success",
+        "user":
+            {
+                "_id": "645b7cc3424d43eee1ea75ff",
+                "first_name": "Cattis",
+                "surname": "Gustafsson",
+				"foods": [{
+					"_id": "AAAA",
+					"title": "Food title",
+					"location": [45.123, 47.232],
+					"free": false,
+					"price": 20,
+					"img": "food.jpg",
+					"expire": ["Tillagningsdatum", Date.now()],
+					"tags": ["vegan", "middag", "hemmagjord", "soppa"],
+					"sold_to": "222222222222222222222222",
+					"conversation": [
+						{
+							"conversation_id": 12345,
+							"time": Date.now(),
+							"buyer": "222222222222222222222222",
+							"messages": [
+								{
+									"sender": "user1",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								}
+							]
+						}
+					]
+            }
+        ]
+    }
+    })
+})
+
+app.post("/conversations/:productID/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "New chat started"
+    })
+})
+
+app.put("/conversations/:productID/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "Update successfull"
+    })
+})
+
+app.put("/conversations/:productID", async (req, res) => {
+    res.status(200).json({
+        "message": "The product is now sold"
+    })
+})
+
+// Konversationer
+app.get("/conversations/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "success",
+        "user":
+            {
+                "_id": "645b7cc3424d43eee1ea75ff",
+                "first_name": "Cattis",
+                "surname": "Gustafsson",
+				"foods": [{
+					"_id": "AAAA",
+					"title": "Food title",
+					"location": [45.123, 47.232],
+					"free": false,
+					"price": 20,
+					"img": "food.jpg",
+					"expire": ["Tillagningsdatum", Date.now()],
+					"tags": ["vegan", "middag", "hemmagjord", "soppa"],
+					"sold_to": "222222222222222222222222",
+					"conversation": [
+						{
+							"conversation_id": 12345,
+							"time": Date.now(),
+							"buyer": "222222222222222222222222",
+							"messages": [
+								{
+									"sender": "user1",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								}
+							]
+						}
+					]
+            }
+        ]
+    }
+    })
+})
+
+// Profil
+app.get("/users/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "success",
+        "user":
+            {
+                "_id": "645b7cc3424d43eee1ea75ff",
+                "first_name": "Cattis",
+                "surname": "Gustafsson",
+                "email": "cattis@hejdev.se",
+				"address": "Arenavägen 63",
+				"img": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+				"foods": [{
+					"_id": "AAAA",
+					"title": "Food title",
+					"desc": "Lorem ipsum",
+					"location": [45.123, 47.232],
+					"free": false,
+					"price": 20,
+					"img": "food.jpg",
+					"expire": ["Tillagningsdatum", Date.now()],
+					"tags": ["vegan", "middag", "hemmagjord", "soppa"],
+					"sold_to": "222222222222222222222222",
+					"conversation": [
+						{
+							"conversation_id": 12345,
+							"time": Date.now(),
+							"buyer": "222222222222222222222222",
+							"messages": [
+								{
+									"sender": "user1",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								}
+							]
+						}
+					]
+            }
+        ]
+    }})
+})
+
+// Edit profil
+app.put("/users/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "success",
+    })
+})
+
+app.delete("/users/:userID", async (req, res) => {
+    res.status(200).json({
+        "message": "delete success",
+    })
+})
+
+// Edit product
+app.put("/products/:productID", async (req, res) => {
+    res.status(200).json({
+        "message": "success",
+    })
+})
+
+app.delete("/products/:productID", async (req, res) => {
+    res.status(200).json({
+        "message": "delete success",
+    })
+})
+
+// New product
+app.post("/products/:productID", async (req, res) => {
+    res.status(200).json({
+        "message": "Product created",
     })
 })
 
