da6990ef986dac43189bc131c06a5167e7c5a2ed
Cattis Gustafsson <cattis@hejdev.se>
2023-05-16 21:53:58 +0200

    Added data and comments to the dummy server

--- dummyServer/server.js
+++ dummyServer/server.js
@@ -64,23 +64,33 @@ app.get("/products/:userID", async (req, res) => {
 })
 
 
-
-app.get("/users", async (req, res) => {
+// Login -> skicka in email och password
+app.post("/login", async (req, res) => {
+	const { email, pass } = req.body
     res.status(200).json({
         "message": "success login",
+		"email": email,
+		"password": pass
     });
 })
 
-app.post("/users", async (req, res) => {
+// Register -> skicka in email, förnamn, efternamn och password
+app.post("/register", async (req, res) => {
+	const { email, firstname, surname, pass } = req.body
     res.status(201).json({
-        "message": "Registration successfull"
+        "message": "Registration successfull",
+		"email": email,
+		"password": pass,
+		"firstname": firstname,
+		"surname": surname
     })
 }) 
 
-app.get("/products/:productID", async (req, res) => {
+// Products excludes own include a fake ID tag -> filter out sold_to 
+app.get("/products/:productID", async (res) => {
     res.status(200).json({
-        "message": "success",
-        "foods": {
+        "message": "Sucess of products",
+        "foods": [{
 			"_id": "AAAA",
 			"title": "Food title",
 			"desc": "Lorem ipsum",
@@ -90,42 +100,49 @@ app.get("/products/:productID", async (req, res) => {
 			"img": "food.jpg",
 			"expire": ["Tillagningsdatum", Date.now()],
 			"tags": ["vegan", "middag", "hemmagjord", "soppa"],
-			"sold_to": "222222222222222222222222",
-			"conversation": [
-				{
-					"conversation_id": 12345,
-					"time": Date.now(),
-					"buyer": "222222222222222222222222",
-					"messages": [
-						{
-							"sender": "user1",
-							"message": "Hello World",
-							"timestamp": Date.now()
-						},
-						{
-							"sender": "user1",
-							"message": "Hello",
-							"timestamp": Date.now()
-						},
-						{
-							"sender": "user2",
-							"message": "Hello World",
-							"timestamp": Date.now()
-						},
-						{
-							"sender": "user1",
-							"message": "Hello",
-							"timestamp": Date.now()
-						}
-					]
-				}
-			]
-	}
+			"sold_to": false,
+		},
+		{
+			"_id": "BBBB",
+			"title": "Another title",
+			"desc": "Another description",
+			"location": [12.345, 67.890],
+			"free": true,
+			"price": 0,
+			"img": "another.jpg",
+			"expire": ["Expiration date", Date.now()],
+			"tags": ["fruit", "snack"],
+			"sold_to": false,
+		},
+		{
+			"_id": "CCCC",
+			"title": "Third title",
+			"desc": "Third description",
+			"location": [98.765, 43.210],
+			"free": false,
+			"price": 10,
+			"img": "third.jpg",
+			"expire": ["Best before", Date.now()],
+			"tags": ["dessert", "chocolate"],
+			"sold_to": "2333333333333333",
+		},
+		{
+			"_id": "DDDD",
+			"title": "Fourth title",
+			"desc": "Fourth description",
+			"location": [23.456, 78.901],
+			"free": true,
+			"price": 0,
+			"img": "fourth.jpg",
+			"expire": ["Expiry date", Date.now()],
+			"tags": ["beverage", "refreshment"],
+			"sold_to": false,
+		}
+	 ]
     })
 })
 
-// Chatten
-
+// Chat
 app.get("/conversations/:productID/:userID", async (req, res) => {
     res.status(200).json({
         "message": "success",
@@ -171,26 +188,84 @@ app.get("/conversations/:productID/:userID", async (req, res) => {
 									"timestamp": Date.now()
 								}
 							]
+						},
+						{
+							"conversation_id": 123112,
+							"time": Date.now(),
+							"buyer": "11111111111",
+							"messages": [
+								{
+									"sender": "user1",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "Hello World",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hello",
+									"timestamp": Date.now()
+								}
+							]
+						},
+						{
+							"conversation_id": 67890,
+							"time": Date.now(),
+							"buyer": "111111111111111111111111",
+							"messages": [
+								{
+									"sender": "user2",
+									"message": "Hi there",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "Hey",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user2",
+									"message": "How are you?",
+									"timestamp": Date.now()
+								},
+								{
+									"sender": "user1",
+									"message": "I'm good, thanks!",
+									"timestamp": Date.now()
+								}
+							]
 						}
 					]
-            }
-        ]
-    }
+            		}
+        		]
+    		}
     })
 })
 
+// Ny chat
 app.post("/conversations/:productID/:userID", async (req, res) => {
     res.status(200).json({
         "message": "New chat started"
     })
 })
 
+// Lägg till en chat
 app.put("/conversations/:productID/:userID", async (req, res) => {
     res.status(200).json({
         "message": "Update successfull"
     })
 })
 
+
+// Klicka på såld knapp
 app.put("/conversations/:productID", async (req, res) => {
     res.status(200).json({
         "message": "The product is now sold"
@@ -251,7 +326,7 @@ app.get("/conversations/:userID", async (req, res) => {
     })
 })
 
-// Profil
+// Profil -> visa inte sålda produkter
 app.get("/users/:userID", async (req, res) => {
     res.status(200).json({
         "message": "success",
@@ -274,36 +349,19 @@ app.get("/users/:userID", async (req, res) => {
 					"expire": ["Tillagningsdatum", Date.now()],
 					"tags": ["vegan", "middag", "hemmagjord", "soppa"],
 					"sold_to": "222222222222222222222222",
-					"conversation": [
-						{
-							"conversation_id": 12345,
-							"time": Date.now(),
-							"buyer": "222222222222222222222222",
-							"messages": [
-								{
-									"sender": "user1",
-									"message": "Hello World",
-									"timestamp": Date.now()
-								},
-								{
-									"sender": "user1",
-									"message": "Hello",
-									"timestamp": Date.now()
-								},
-								{
-									"sender": "user2",
-									"message": "Hello World",
-									"timestamp": Date.now()
-								},
-								{
-									"sender": "user1",
-									"message": "Hello",
-									"timestamp": Date.now()
-								}
-							]
-						}
-					]
-            }
+            }, 
+			{
+				"_id": "DDDD",
+				"title": "Fourth title",
+				"desc": "Fourth description",
+				"location": [23.456, 78.901],
+				"free": true,
+				"price": 0,
+				"img": "fourth.jpg",
+				"expire": ["Expiry date", Date.now()],
+				"tags": ["beverage", "refreshment"],
+				"sold_to": false,
+			}
         ]
     }})
 })
