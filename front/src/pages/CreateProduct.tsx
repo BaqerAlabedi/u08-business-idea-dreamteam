91f919cd6a89f6c3fa941c132caedc8fa8beb027
Cattis Gustafsson <cattis@hejdev.se>
2023-05-17 12:19:06 +0200

    Merge all Saliems pages + fix routes

---
+++ front/src/pages/CreateProduct.tsx
@@ -0,0 +1,62 @@
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { BsArrowLeft } from "react-icons/bs";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
+function CreateProduct() {
+	return (
+		<div className="m-0 min-[350px]:m-9">
+			<div className="flex justify-center items-center my-4">
+				<div className="min-[800px]:mr-72">
+					<a href="/">
+						<button className="lg:hidden mr-20 max-[350px]:mr-22">
+							<BsArrowLeft size={25}></BsArrowLeft>
+						</button>
+						<button className="max-[800px]:hidden">
+							<MdKeyboardArrowLeft size={40}></MdKeyboardArrowLeft>
+						</button>
+					</a>
+				</div>
+				<h2 className="flex min-[800px]:mr-72 text-xl font-bold mr-28">Ny annons</h2>
+			</div>
+			<div className="flex justify-center items-center mb-10">
+				<div className="flex justify-center items-center">
+					<form className="lg:flex min-[320px]:block">
+						<div className="lg:mr-20">
+							<Input placeHolder={""} inputID={"titel"} labelText={"Rubrik"}></Input>
+							<Input placeHolder={""} inputID={"description"} labelText={"Beskrivning"}></Input>
+						</div>
+
+						<div>
+							<label htmlFor="adress" className="block">
+                                Address
+								<span className="text-neutral-400 text-sm"> (syns ej för andra användare)</span>
+							</label>
+							<input type="textarea" id="adress" className="box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+							<Input placeHolder={"Välj bild..."} inputID={"image"} labelText={"Bild"}></Input>
+
+							<label htmlFor="price" className="block my-2">Pris</label>
+							<input type="checkbox" id="give-away"/>
+							<label htmlFor="give-away" className="text-sm mx-2">Bortskänkes</label>
+							<input type="number" id="price" className="block my-2 box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+							<label htmlFor="dates" className="block my-2">Tillagning/utgångsdatum</label>
+							<section id="dates" className="block my-2 px-5 box-border h-11 rounded border-solid border-gray-300 border">
+								<option value="tillagning">Tillagningsdatum</option>
+								<option value="utgångsdatum">Utgångsdatum</option>
+							</section>
+							<input type="date" id="dates" className="block mt-2 mb-5 px-6 box-border h-11 rounded border-solid border-gray-300 border"/>
+
+							<div className="block">
+								<Button children={"Spara annons"} className={""}></Button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CreateProduct;
---
+++ front/src/pages/EditProduct.tsx
@@ -0,0 +1,68 @@
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { BsArrowLeft } from "react-icons/bs";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
+function EditProduct() {
+	return (
+		<div className="m-8">
+			<div className="block float-left">
+				<a href="/">
+					<button className="min-[800px]:hidden">
+						<BsArrowLeft size={25}></BsArrowLeft>
+					</button>
+					<button className="max-[800px]:hidden">
+						<MdKeyboardArrowLeft size={40}></MdKeyboardArrowLeft>
+					</button>
+				</a>
+			</div>
+			<div className="flex justify-center items-center my-2">
+				<div className="flex">
+					<h2 className="text-xl font-bold">
+                    Ändra annons</h2>
+				</div>
+			</div>
+			<div className="flex justify-center items-center mb-10">
+				<div className="flex justify-center items-center">
+					<form className="lg:flex min-[320px]:block">
+						<div className="lg:mr-20">
+							<Input placeHolder={""} inputID={"titel"} labelText={"Rubrik"}></Input>
+							<Input placeHolder={""} inputID={"description"} labelText={"Beskrivning"}></Input>
+						</div>
+						<div>
+							<label htmlFor="adress" className="block">
+                                Address
+								<span className="text-neutral-400 text-sm"> (syns ej för andra användare)</span>
+							</label>
+							<input type="textarea" id="adress" className="box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+							<Input placeHolder={"Välj bild..."} inputID={"image"} labelText={"Bild"}></Input>
+
+							<label htmlFor="price" className="block my-2">Pris</label>
+							<input type="checkbox" id="give-away"/>
+							<label htmlFor="give-away" className="text-sm mx-2">Bortskänkes</label>
+							<input type="number" id="price" className="block my-2 box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+							<label htmlFor="dates" className="block my-2">Tillagning/utgångsdatum</label>
+							<select id="dates" className="block my-2 px-5 box-border h-11 rounded border-solid border-gray-300 border">
+								<option value="tillagning">Tillagningsdatum</option>
+								<option value="utgångsdatum">Utgångsdatum</option>
+							</select>
+							<input type="date" id="dates" className="block mt-2 mb-5 px-6 box-border h-11 rounded border-solid border-gray-300 border"/>
+
+							<div className="flex justify-center min-[800px]:block">
+								<Button children={"Uppdatera annons"} className={""}></Button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<hr className="w-full" />
+			<div className="flex justify-center my-5">
+				<Button children={"Radera annons"} className={""}></Button>
+			</div>
+		</div>
+	);
+}
+
+export default EditProduct;
