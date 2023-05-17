ed6d8519580e2e015f009138b30953d70e330ce5
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-05-17 15:14:10 +0200

    created

--- dummyServer/server.js
+++ dummyServer/server.js
@@ -160,6 +160,7 @@ app.get("/products/:productsID", async (req, res) => {
 			"price": 20,
 			"img": "https://images.unsplash.com/photo-1546069901-ba9599a7e63c",
 			"expire": ["Tillagningsdatum", Date.now()],
+			"created": Date.now() - 1000000,
 			"tags": ["vegan", "middag", "hemmagjord", "soppa"],
 			"sold_to": false,
 		}
