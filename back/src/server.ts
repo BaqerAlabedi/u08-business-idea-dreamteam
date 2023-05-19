6b90d81f7ef1423dcaf357920ea3cce0fd6eb250
William Carlström <vbw123@live.se>
2023-05-19 15:03:42 +0200

    created server

---
+++ back/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import {userRouter} from "./routes/routes";
+import "./db/conn";
+
+const port = 4000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
+
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
