cda855de45833fe81fe1150847657a0bba2118ba
Cattis Gustafsson <cattis@hejdev.se>
2023-05-21 12:40:42 +0200

    Fixed styling

--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -52,7 +52,7 @@ function Register() {
 	return (
 		<div className="absolute top-0 flex items-center justify-center h-screen w-screen bg-cover bg-center bg-register ">
 			<div className="rounded-md md:w-1/3 lg:bg-white/80 lg:p-12 ">
-				<div className="flex flex-col justify-center mb-12">
+				<div className="flex flex-col justify-center">
 					<div className="mx-auto">
 						<h1 className="text-lg w-72 font-bold">Registrera dig</h1>
 						<h2 className="w-72 mb-2">till GrannskapsRätten</h2>
