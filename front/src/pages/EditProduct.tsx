50359b7837e75284a0c8f461c529f2132af477eb
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-05-24 10:57:12 +0200

    starting with editproduct

--- front/src/pages/EditProduct.tsx
+++ front/src/pages/EditProduct.tsx
@@ -1,9 +1,66 @@
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useState, useEffect } from "react";
 import { BsArrowLeft } from "react-icons/bs";
 import { MdKeyboardArrowLeft } from "react-icons/md";
+import { updateOneProduct, getOneProduct } from "../functions/api";
+import { useParams } from "react-router-dom";
+
+type ProductParam = {
+	productID: string
+}
+
+export interface FoodResponse {
+	foods: [{
+		_id: string,
+		title: string,
+		desc: string,
+		location: [number, number],
+		free: boolean,
+		price: number,
+		img: string,
+		expire: [string, number],
+		tags: string[],
+		created: number,
+		sold_to: boolean,
+	}
+ ]
+}
 
 function EditProduct() {
+
+	const [data, setData] = useState<FoodResponse | null>(null);
+	const { productID } = useParams<ProductParam>();
+
+	useEffect(() => {
+		if(productID) {
+			const fetchData = async () => {
+				try {
+					const response = await getOneProduct(productID);
+					console.log(response);
+					setData(response);
+				} catch (error) {
+					console.error(error);
+				}
+			};
+			fetchData();
+		}
+	}, []);
+
+	const handleClick = () => {
+		if(productID) {
+			const fetchData = async () => {
+				try {
+					const response = await updateOneProduct(productID);
+					console.log(response);
+				} catch (error) {
+					console.error(error);
+				}
+			};
+			fetchData();
+		}
+	};
+
 	return (
 		<div className="m-8">
 			<div className="block float-left">
@@ -22,41 +79,43 @@ function EditProduct() {
                     Ändra annons</h2>
 				</div>
 			</div>
-			<div className="flex justify-center items-center mb-10">
-				<div className="flex justify-center items-center">
-					<form className="lg:flex min-[320px]:block">
-						<div className="lg:mr-20">
-							<Input placeHolder={""} inputID={"titel"} labelText={"Rubrik"}></Input>
-							<Input placeHolder={""} inputID={"description"} labelText={"Beskrivning"}></Input>
-						</div>
-						<div>
-							<label htmlFor="adress" className="block">
-                                Address
-								<span className="text-neutral-400 text-sm"> (syns ej för andra användare)</span>
-							</label>
-							<input type="textarea" id="adress" className="box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
-
-							<Input placeHolder={"Välj bild..."} inputID={"image"} labelText={"Bild"}></Input>
-
-							<label htmlFor="price" className="block my-2">Pris</label>
-							<input type="checkbox" id="give-away"/>
-							<label htmlFor="give-away" className="text-sm mx-2">Bortskänkes</label>
-							<input type="number" id="price" className="block my-2 box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
-
-							<label htmlFor="dates" className="block my-2">Tillagning/utgångsdatum</label>
-							<select id="dates" className="block my-2 px-5 box-border h-11 rounded border-solid border-gray-300 border">
-								<option value="tillagning">Tillagningsdatum</option>
-								<option value="utgångsdatum">Utgångsdatum</option>
-							</select>
-							<input type="date" id="dates" className="block mt-2 mb-5 px-6 box-border h-11 rounded border-solid border-gray-300 border"/>
-
-							<div className="flex justify-center min-[800px]:block">
-								<Button children={"Uppdatera annons"} className={""}></Button>
+			{data && (
+				<div className="flex justify-center items-center mb-10">
+					<div className="flex justify-center items-center">
+						<form className="lg:flex min-[320px]:block">
+							<div className="lg:mr-20">
+								<Input placeHolder={""} inputID={"titel"} labelText={"Rubrik"}></Input>
+								<Input placeHolder={""} inputID={"description"} labelText={"Beskrivning"}></Input>
 							</div>
-						</div>
-					</form>
+							<div>
+								<label htmlFor="adress" className="block">
+                                Address
+									<span className="text-neutral-400 text-sm"> (syns ej för andra användare)</span>
+								</label>
+								<input type="textarea" id="adress" className="box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+								<Input placeHolder={"Välj bild..."} inputID={"image"} labelText={"Bild"}></Input>
+
+								<label htmlFor="price" className="block my-2">Pris</label>
+								<input type="checkbox" id="give-away"/>
+								<label htmlFor="give-away" className="text-sm mx-2">Bortskänkes</label>
+								<input type="number" id="price" className="block my-2 box-border h-11 w-72 rounded border-solid border-gray-300 border"/>
+
+								<label htmlFor="dates" className="block my-2">Tillagning/utgångsdatum</label>
+								<select id="dates" className="block my-2 px-5 box-border h-11 rounded border-solid border-gray-300 border">
+									<option value="tillagning">Tillagningsdatum</option>
+									<option value="utgångsdatum">Utgångsdatum</option>
+								</select>
+								<input type="date" id="dates" className="block mt-2 mb-5 px-6 box-border h-11 rounded border-solid border-gray-300 border"/>
+
+								<div className="flex justify-center min-[800px]:block">
+									<Button onClick={handleClick} children={"Uppdatera annons"} className={""}></Button>
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
-			</div>
+			)}
 			<hr className="w-full" />
 			<div className="flex justify-center my-5">
 				<Button children={"Radera annons"} className={""}></Button>
