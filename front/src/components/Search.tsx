3fa84db885db861e64817a2de620bb36c6b58f89
salleeb <saliem.brhane@chasacademy.se>
2023-05-26 23:46:00 +0200

    cleaned up code

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 import { BsFilter } from "react-icons/bs";
 
-export const Search = (props: { name?: string | undefined, filtered?: true, setFilterValue?: any, onClick: (arg0:string) => void, value: string, value1?: string, value2?: string, value3?: string, value4?: string, value5?: string, value6?: string;} ) => {
+export const Search = (props: { name?: string | undefined, filtered?: true, onClick: (arg0:string) => void} ) => {
 	const [visible, setVisible] = useState(false);
 
 	return (
@@ -30,22 +30,26 @@ export const Search = (props: { name?: string | undefined, filtered?: true, setF
 								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("snack")} value="snack"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => {props.setFilterValue("starter"); props.onClick();}} value={props.value1}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("starter")} value="starter"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={props.onClick} value={props.value2}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("soup")} value="soup"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={props.onClick} value={props.value3}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("sallad")} value="sallad"/>
+							</li>
+							<li></li>
+							<li>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("main course")} value="main course"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={props.onClick} value={props.value4}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("vegetarian")} value="vegetarian"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={props.onClick} value={props.value5}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("vegan")} value="vegan"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("vegan")} value={props.value6}/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("dessert")} value="dessert"/>
 							</li>
 						</ul>
 					</div>
