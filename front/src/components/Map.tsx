49f06d84aa42bffcdac4a8b146c200c46299df68
salleeb <saliem.brhane@chasacademy.se>
2023-05-26 14:09:37 +0200

    geolocation zustand

--- front/src/components/Map.tsx
+++ front/src/components/Map.tsx
@@ -1,16 +1,51 @@
 import {GoogleMap, useJsApiLoader} from "@react-google-maps/api";
+import { useEffect } from "react";
+import { GeolocationStore } from "../storage/GeolocationStore";
 
 
 export default function Map() {
+	const { location, error, loading, setLocation, setError, setLoading } = GeolocationStore();
+
+	useEffect(() => {
+		setLoading(true);
+
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const lat = position.coords.latitude;
+				const lng = position.coords.longitude;
+				const geoPosition = lat + "," + lng;
+				setLocation(geoPosition);
+				setLoading(false);
+			},
+			() => {
+				setError("Unable to retrieve your location");
+				setLoading(false);
+			}
+		);
+
+		if (!navigator.geolocation) {
+			setError("Geolocation is not supported by your browser!");
+			setLoading(false);
+		}
+	}, []);
+
+	console.log(location);
+	console.log("Loading..." + loading);
+	console.log(error);
+
 	const {isLoaded} = useJsApiLoader({
 		googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
 	});
 	if(!isLoaded) return <div>Loading...</div>;
+
 	return(
 		<>
 			<GoogleMap zoom={10} center={{lat: 59.33, lng: 18.06}} mapContainerClassName="w-screen h-96">
-
 			</GoogleMap>
+
+			{/* {loading && <p>Loading...</p>}
+			{error && <p>{error}</p>} */}
+			{/* {location && <p>{location}</p>} */}
 		</>
 
 	);
--- front/src/storage/GeolocationStore.tsx
+++ front/src/storage/GeolocationStore.tsx
@@ -1,90 +1,19 @@
-import { useState } from "react";
 import { create } from "zustand";
-import Dashboard from "../pages/Dashboard";
 
-// const url = "http://localhost:4000/products";
+type LocationStore = {
+	location: string | null;
+	error: string | null;
+	loading: boolean;
+	setLocation: (location: string) => void;
+	setError: (error: string) => void;
+	setLoading: (loading: boolean) => void;
+  };
 
-export const GeolocationStore = create((set)) => {
-	handleLocation: ()
-
-
-// 	const [lat, setLat] = useState(null);
-//   const [lng, setLng] = useState(null);
-//   const [status, setStatus] = useState(null);
-
-//   const getLocation = () => {
-//     if(!navigator.geolocation) {
-//       setStatus("Geolocation is not supported by your browser!");
-//     } else {
-//       setStatus("Loading...");
-//     }
-
-//     navigator.geolocation.getCurrentPosition(
-//       (position) => {
-//         setStatus("");
-//         setLat(position.coords.latitude);
-//         setLng(position.coords.longitude);
-//       },
-//       () => {
-//         setStatus("Unable to retrieve your location");
-//       }
-//     );
-//   };
-
-	// handleLocationClick: async () => {
-	// 		if (navigator.geolocation) {
-	// 			status("Loading...");
-	// 		}
-	
-	// 		navigator.geolocation.getCurrentPosition(
-	// 			async (position) => {
-	// 				const lat = position.coords.latitude;
-	// 				const lng = position.coords.longitude;
-	// 				const curPosition = lat + "," + lng;
-	
-	// 				const resp = await fetch((curPosition));
-	// 				const data = await resp.json();
-	// 				status("");
-	// 				location(data);
-	// 			},
-	// 			() => {
-	// 				return status("Unable to retrieve your location. Tap Allow to let the app use location information as needed");
-	// 			}
-	// 		);
-	// 	};
-
-	// const [status, setStatus] = useState("");
-	// const [location, setLocation] = useState("");
-
-	// const handleLocationClick = async () => {
-	// 	if (navigator.geolocation) {
-	// 		setStatus("Loading...");
-	// 	}
-
-	// 	navigator.geolocation.getCurrentPosition(
-	// 		async (position) => {
-	// 			const lat = position.coords.latitude;
-	// 			const lng = position.coords.longitude;
-	// 			const curPosition = lat + "," + lng;
-
-	// 			const resp = await fetch(url(curPosition));
-	// 			const data = await resp.json();
-	// 			setStatus("");
-	// 			setLocation(data);
-	// 		},
-	// 		() => {
-	// 			return setStatus("Unable to retrieve your location. Tap Allow to let the app use location information as needed");
-	// 		}
-	// 	);
-	// };
-
-    // handleLocationClick();
-
-	// return (
-	// 	<>
-	// 		<div>
-	// 			{status ? ( <p className="flex justify-center text-xl">{status}</p> ) : ( <></> )}
-	// 		</div>
-	// 	</>
-	// )
-};
+export  const GeolocationStore = create<LocationStore>((set) => ({
+	location: null,
+	error: null,
+	loading: false,
+	setLocation: (location) => set({ location }),
+	setError: (error) => set({ error }),
+	setLoading: (loading) => set({ loading }),
+}));
