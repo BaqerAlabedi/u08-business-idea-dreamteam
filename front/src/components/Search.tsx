b94b63037e42afcae7a8e27a1ac633ba89feaa45
salleeb <saliem.brhane@chasacademy.se>
2023-05-28 17:39:07 +0200

    fixed errors

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 import { BsFilter } from "react-icons/bs";
 
-export const Search = (props: { name?: string | undefined, reset?: () => void, filtered?: true, onClick?: (arg0:string) => void } ) => {
+export const Search = (props: { name?: string | undefined, reset?: (arg0:string) => void, filtered?: true, onClick?: (arg0:string) => void } ) => {
 	const [visible, setVisible] = useState(false);
 
 	return (
 		<div className="search">
 			<div className="flex justify-center align-center">
 				{props.filtered && (
-					<button type='button' className="px-3 py-1" onClick={() => { setVisible(!visible); props.reset(); }}>
+					<button type='button' className="px-3 py-1" onClick={() => {
+						setVisible(!visible);
+						if (props.reset) {
+							props.reset("resetFilter");
+						}
+					}}>
 						<BsFilter size={25}></BsFilter>
 					</button>
 				)}
@@ -27,29 +32,68 @@ export const Search = (props: { name?: string | undefined, reset?: () => void, f
 					<div className="flex justify-center">
 						<ul className="flex">
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("snack")} value="snack"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("snack");
+										}
+									}} value="snack"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("starter")} value="starter"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("starter");
+										}
+									}} value="starter"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("soup")} value="soup"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("soup");
+										}
+									}} value="soup"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("sallad")} value="sallad"/>
-							</li>
-							<li></li>
-							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("main course")} value="main course"/>
-							</li>
-							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("vegetarian")} value="vegetarian"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("salad");
+										}
+									}} value="salad"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("vegan")} value="vegan"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("main course");
+										}
+									}} value="main course"/>
 							</li>
 							<li>
-								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white" onClick={() => props.onClick("dessert")} value="dessert"/>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("vegetarian");
+										}
+									}} value="vegetarian"/>
+							</li>
+							<li>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("vegan");
+										}
+									}} value="vegan"/>
+							</li>
+							<li>
+								<input type="button" className="mx-2 mt-2 mb-5 px-6 rounded-lg font-medium text-black border-2 border-teal-700 hover:bg-teal-700 hover:text-white"
+									onClick={() => {
+										if (props.onClick) {
+											props.onClick("dessert");
+										}
+									}} value="dessert"/>
 							</li>
 						</ul>
 					</div>
