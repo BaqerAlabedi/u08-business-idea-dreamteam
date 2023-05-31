328aff19387159e8d25f944c35762937859eccb8
Cattis Gustafsson <cattis@hejdev.se>
2023-05-31 12:57:12 +0200

    Fix in ads

--- front/src/components/Advertisement.tsx
+++ front/src/components/Advertisement.tsx
@@ -4,6 +4,7 @@ export const Advertisement = () => {
 	return (
 		<>
 			<ProductShow
+				to="/dashboard"
 				imgUrl={"https://www.affarsvarlden.se/wp-content/uploads/2020/10/hellofresh-900.jpg"}
 				title={"HelloFresh"}
 				description={"Spara på dina 5 första kassar!"}
