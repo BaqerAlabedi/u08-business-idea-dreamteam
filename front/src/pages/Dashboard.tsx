1d3eae020deb4559c510a48ec086d6230f65c8f8
salleeb <saliem.brhane@chasacademy.se>
2023-06-02 00:06:35 +0200

    fixed filter out users own products

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -7,6 +7,7 @@ import ProductShow from "../components/ProductShow";
 import Search from "../components/Search";
 import { Advertisement } from "../components/Advertisement";
 import { getProducts } from "../functions/api";
+import useStoreUser from "../storage/UserStorage";
 
 export interface Products {
 	foods: any;		// Fixa any!
@@ -19,32 +20,19 @@ export interface Products {
     img: string;
 }
 
-
 export default function Dashboard(this: unknown) {
 	const [data,  setData] = useState<Products[]>([]);
 	const [filteredProducts, setFilteredProducts] = useState<Products[]>([]);
 	const [filterActive, setFilterActive] = useState(false);
 	const [activeFilterProducts, setActiveFilterProducts] = useState(false);
+	const { storeUser } = useStoreUser();
 
 	const genGetAllProducts = async () => {
-		// FIXA FILTRERA UT ANVÄNDAREN PRODUKTER!
 		const res = await getProducts();
-		// const test = res.map(item => item._id);
-		// const filteredUser = res.filter(item => item.test !== item.test);
-		// console.log(test);
-		// console.log(filteredUser);
-		// const filteredUser = res.filter((item: { _id: unknown; }) => item._id !== console.log(item._id));
-		// console.log(filteredUser)
-		// const test = res.map((res) => res.foods);
-		// const foodItems = res.map(item =>item)
-		// const foodItems = foodUsers.filter((item: { foods: any; }) => console.log(item.foods));
-
-		const products = res.flatMap((item: { foods: unknown; }) => item.foods);
-
-		console.log(products);
+		const filteredUser = res.filter((user: { _id: unknown; }) => user._id !== storeUser);
+		const products = filteredUser.flatMap((item: { foods: unknown; }) => item.foods);
 		setData(products);
 	};
-	console.log(data);
 
 	useEffect(() => {
 		genGetAllProducts();
@@ -53,16 +41,14 @@ export default function Dashboard(this: unknown) {
 	const handleClick = async (filter:string) => {
 		setData([]);
 		const res = await getProducts();
-		const products = res.map((res: { foods: unknown; }) => res.foods);
+		const filteredUser = res.filter((user: { _id: unknown; }) => user._id !== storeUser);
+		const products = filteredUser.map((res: { foods: unknown; }) => res.foods);
 		const filteredItems = products.flat().filter((item: { tags: string | string[]; }) => item.tags && item.tags.includes(filter));
 		setFilteredProducts(filteredItems);
 
 		setActiveFilterProducts(true);
 		if (activeFilterProducts) {
-			const test = res.map((item: { foods: unknown; }) => item.foods);
-			const filteredItems = test.flat().filter((item: { tags: string | string[]; }) => item.tags && item.tags.includes(filter));
 			setFilteredProducts(filteredItems);
-			console.log(filteredItems);
 		}
 	};
 
@@ -72,7 +58,7 @@ export default function Dashboard(this: unknown) {
 		genGetAllProducts();
 	};
 
-	return(
+	return (
 		<>
 			<Map></Map>
 
@@ -85,7 +71,7 @@ export default function Dashboard(this: unknown) {
 
 			{data && <section className="w-10/12 max-w-7xl mx-auto my-4 grid col-auto gap-5 lg:grid-cols-2">
 
-				{ data.map((item, idx) =>
+				{ data.map((item,  idx) =>
 					<React.Fragment key={item._id}>
 						<section>
 							<ProductShow
@@ -98,7 +84,7 @@ export default function Dashboard(this: unknown) {
 								distance={1.2}		// Location är temporärt!
 							></ProductShow>
 						</section>
-						{ (idx !== 0 && idx % 3 === 0) && <><section><Advertisement/></section></> }
+						{ (idx < 0 && idx % 3 === 0) && <><section><Advertisement/></section></> }
 					</React.Fragment>
 				)}
 
@@ -115,7 +101,7 @@ export default function Dashboard(this: unknown) {
 								distance={1.2}		// Location är temporärt!
 							></ProductShow>
 						</section>
-						{ (idx !== 0 && idx % 3 === 0) && <><section><Advertisement/></section></> }
+						{ (idx < 0 && idx % 3 === 0) && <><section><Advertisement/></section></> }
 					</React.Fragment>
 				)
 				)}
