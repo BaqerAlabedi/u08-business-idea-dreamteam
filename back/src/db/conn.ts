3c7ed656cd153717ec3b129f967fde4037013283
BaqerAlabedi <112864227+BaqerAlabedi@users.noreply.github.com>
2023-06-07 16:33:07 +0200

    Reverted env

--- back/src/db/conn.ts
+++ back/src/db/conn.ts
@@ -1,21 +1,14 @@
-// import mongoose, {ConnectOptions} from "mongoose";
-// require("dotenv").config();
-
-
-// const DB = process.env.DB_URL;
-
-// if(DB){
-// 	mongoose.connect(DB, {
-// 		useNewUrlParser: true,
-// 	} as ConnectOptions);
-// }
-// else {
-// 	console.log("ENV variable is undefined");
-// }
 import mongoose, {ConnectOptions} from "mongoose";
+require("dotenv").config();
 
-const DB = "mongodb://localhost:27017/GrannskapsRatten";
 
-mongoose.connect(DB, {
-	useNewUrlParser: true,
-} as ConnectOptions);
+const DB = process.env.DB_URL;
+
+if(DB){
+	mongoose.connect(DB, {
+		useNewUrlParser: true,
+	} as ConnectOptions);
+}
+else {
+ 	console.log("ENV variable is undefined");
+}
